9bb0ce7d9a273daead3c048e0dded07fda7d67dc
Flint270 <45808843+Flint270@users.noreply.github.com>
2019-04-11 21:52:14 -0400

    WASD movement setup

---
+++ FPS base/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PlayerInput : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    PlayerMovement movement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        movement = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.translation = Input.GetAxis("Vertical") * speed;
+        movement.straffe = Input.GetAxis("Horizontal") * speed;
+        
+
+        if(Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
---
+++ FPS base/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerInput))]
+[DisallowMultipleComponent]
+public class PlayerMovement : MonoBehaviour
+{
+    public float translation;
+    public float straffe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+    }
+}
